83c668754a202b44df3da6e8767af36db2ed43b5
wenb1125 <2424624564@qq.com>
2021-03-03 11:24:03 +0800

    增加区块链结构体定义，封装区块链的功能方法

---
+++ chain/blockchain.go
@@ -0,0 +1,31 @@
+package chain
+
+/**
+ * 定义区块链结构体，用于存储产生的区块（内存中）
+ */
+type BlockChain struct {
+	Blocks []Block
+}
+
+func NewBlockChain() BlockChain {
+	return BlockChain{}
+}
+
+/**
+ * 创建一个区块链实例，该实例携带一个创世区块
+ */
+func CreatChainWithGenesis(genesisData []byte) BlockChain {
+	genesis := CreateGenesisBlock(genesisData)
+	blocks := make([]Block, 0)
+	blocks = append(blocks, genesis)
+	return BlockChain{blocks}
+}
+
+func (chain *BlockChain) AddNewBlock(data []byte) {
+	//1.找到切片的最后一个元素，代表最新的区块
+	lastBlock := chain.Blocks[len(chain.Blocks)-1]
+	//2.根据最后一个区块产生一个新区块
+	newBlock := CreateBlock(lastBlock.Height, lastBlock.Hash, data)
+	//3.把最新产生的区块放入到切片中
+	chain.Blocks = append(chain.Blocks, newBlock)
+}
--- main.go
+++ main.go
@@ -4,6 +4,16 @@ import (
 	"fmt"
 	"xianfengChain/chain"
 )
+/**
+ * 步骤
+ * 1.数据结构定义
+ * 2.产生区块
+ * 3.产生多个区块
+ * 4.将区块连起来
+		a.把已有的区块存起来
+ * 5.把区块持久存起来
+ * 6.多个节点直接同步数据
+ */
 
 /**
  * 项目的主入口
@@ -11,12 +21,23 @@ import (
 func main() {
 	fmt.Println("hello!!!")
 
-	gensis := chain.CreateGenesisBlock([]byte("hello"))
-	fmt.Println("区块0: ", gensis)
-	block1 := chain.CreateBlock(gensis.Height,gensis.Hash,nil)
-	fmt.Println("区块1: ", block1)
-	block2 := chain.CreateBlock(gensis.Height,block1.Hash,nil)
-	fmt.Println("区块2: ", block2)
+	blockchain := chain.CreatChainWithGenesis([]byte("hello"))
+
+	blockchain.AddNewBlock([]byte("block1"))
+	blockchain.AddNewBlock([]byte("block2"))
+	fmt.Println("当前共有区块个数： ", len(blockchain.Blocks))
+	fmt.Println(blockchain.Blocks[0])
+	fmt.Println(blockchain.Blocks[1])
+	fmt.Println(blockchain.Blocks[2])
+
+
+
+	//gensis := chain.CreateGenesisBlock([]byte("hello"))
+	//fmt.Println("区块0: ", gensis)
+	//block1 := chain.CreateBlock(gensis.Height,gensis.Hash,nil)
+	//fmt.Println("区块1: ", block1)
+	//block2 := chain.CreateBlock(gensis.Height,block1.Hash,nil)
+	//fmt.Println("区块2: ", block2)
 }
 
 
